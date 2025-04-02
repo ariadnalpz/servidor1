7039825f797b8543d2070bedf7ced9c9c513bcd3
ariadnalpz <ariadna.lopez2904@gmail.com>
2025-04-02 13:16:09 -0600

    Se agrego el nombre del dominio

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const cors = require('cors'); // Importa el paquete cors
+const cors = require('cors');
 const rateLimit = require('./middleware/rateLimit');
 const authRoutes = require('./routes/auth');
 require('dotenv').config();
 
 const app = express();
 
-// Configura CORS para permitir solicitudes desde http://localhost:3000
-app.use(cors());
+// Configura CORS para permitir solicitudes desde el dominio de tu frontend
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://frontend-teal-six-25.vercel.app'], // Cambia 'https://tufrontend.com' por el dominio real de tu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json());
 app.use(rateLimit); // Aplica Rate Limit globalmente
